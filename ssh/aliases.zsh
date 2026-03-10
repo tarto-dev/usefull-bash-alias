468b434b235fd91b60b6baa5b07625b2056878ba
Clara Cassinat <clara.cassinat@gmail.com>
2026-03-10 13:43:09 +0100

    ✨ feat(ssh): add installux prod and preprod aliases

--- ssh/aliases.zsh
+++ ssh/aliases.zsh
@@ -6,3 +6,5 @@
 # Example:
 # alias ssh-myproject-prod="ssh -i ~/.ssh/id_ecdsa user@host -p 22"
 # alias ssh-myproject-preprod="ssh -i ~/.ssh/id_ecdsa user@host -p 22"
+alias ssh-installux-prod="ssh -i ~/.ssh/id_ecdsa installux-prod-site@bastion-ssh.fr-01.cloud.alfa-safety.net -p 2222"
+alias ssh-installux-preprod="ssh -i ~/.ssh/id_ecdsa installux-preprod-site@bastion-ssh.fr-01.cloud.alfa-safety.net -p 2222"
